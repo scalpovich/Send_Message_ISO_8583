17f55384412ddaccef5e87a41adb061510b8efd1
chunhatminh01 <82219047+chunhatminh01@users.noreply.github.com>
2022-07-15 15:34:04 +0700

    asyncCongig

---
+++ src/main/java/com/opw/financemesage/config/AsyncConfiguration.java
@@ -0,0 +1,26 @@
+package com.opw.financemesage.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+public class AsyncConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AsyncConfiguration.class);
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        LOGGER.debug("Creating Async Task Executor");
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("CarThread-");
+        executor.initialize();
+        return executor;
+    }
+
+}
