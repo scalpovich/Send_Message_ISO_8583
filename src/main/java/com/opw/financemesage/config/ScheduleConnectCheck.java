13c16f3ec6ac23295825c17ce643afe498ccbb2d
chunhatminh01 <82219047+chunhatminh01@users.noreply.github.com>
2022-07-19 11:35:37 +0700

    Multi-Threading processing && scheduled Conneted check

--- src/main/java/com/opw/financemesage/config/ScheduleConnectCheck.java
+++ src/main/java/com/opw/financemesage/config/ScheduleConnectCheck.java
@@ -1,6 +1,8 @@
 package com.opw.financemesage.config;
 
 import com.opw.financemesage.socket.SocketIO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,9 +13,14 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class ScheduleConnectCheck {
     @Autowired
     private SocketIO socketIO;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleConnectCheck.class);
     @Scheduled(fixedRate = 5000)
     public void checkConnected() {
-        if(!socketIO.getSocket().isConnected())
+        if(!socketIO.getSocket().isConnected()) {
+            LOGGER.info("Socket is disconnected. Trying to reconnect");
             socketIO = new SocketIO();
+        }
+//        else
+//            LOGGER.info("Socket is connecting");
     }
 }
