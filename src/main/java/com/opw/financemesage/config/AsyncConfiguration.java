094c5097fb62fe5a8a31da526dd69fce322722a2
chunhatminh01 <82219047+chunhatminh01@users.noreply.github.com>
2022-07-22 11:40:27 +0700

    update validation

--- src/main/java/com/opw/financemesage/config/AsyncConfiguration.java
+++ src/main/java/com/opw/financemesage/config/AsyncConfiguration.java
@@ -18,8 +18,8 @@ public class AsyncConfiguration {
     public Executor taskExecutor() {
         LOGGER.debug("Creating Async Task Executor");
         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(5);
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("MessageThread-");
         executor.initialize();
